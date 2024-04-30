027112fad0e0b4654fb68cf336e7e28f17554f32
Dhanushri s <90515404+Dhanushri-S@users.noreply.github.com>
2024-04-30 21:24:37 +0530

    first commit

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {HERO_CONTENT} from "../constants"
+import  profilePic from "../assets/kevinRushProfile.png"
+import { delay, motion } from "framer-motion"
+
+const container = (delay) => ({
+    hidden :{ x: -100, opacity: 0},
+    visible :{ x:0 ,
+        opacity: 1, 
+        transition : {duration: 1, delay :delay}
+    }
+
+})
+
+
+const Hero = () => {
+return (
+<div className='border-b border-neutral-900 pb-4 lg:mb-35'>
+    <div className='flex flex-wrap'>
+        <div className='w-full lg:w-1/2 '>
+            <div 
+            className='flex flex-col items-center lg:items-start'>
+                <motion.h1 initial={{x:-100, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:1, delay:0}}
+                 className='pb-16 text-5xl font-thin tracking-tight lg:mt-16 lg:text-8xl '>
+                    Dhanushri S
+                </motion.h1>
+                <motion.span initial={{x:-100, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:1, delay:0.5}}
+                        className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
+                        bg-clip-text text-4xl  tracking-tight text-transparent'>Front-End Developer
+                </motion.span>
+                <motion.p initial={{x:-100, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:1, delay:1}}  className='my-2 max-w-xl py-6 font-light tracking-tight'> {HERO_CONTENT}</motion.p>
+            </div>
+            
+        </div>
+        <div className='w-full lg:w-1/2 lg:p-8'>
+                <div className='flex justify-center'>
+                    <motion.img initial={{x:100, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:1, delay:1.2}} src={profilePic} alt="profilePic" className='w-96' />
+                </div>
+            </div>
+
+    </div>
+</div>
+)
+}
+
+export default Hero
---
+++ src/constants/index.js
@@ -0,0 +1,59 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/food_2.png";
+import project3 from "../assets/projects/project-3.jpg";
+ 
+
+export const HERO_CONTENT = `I am a passionate Front-End developer with a knack for crafting robust and scalable web applications. With 1.5 years of hands-on experience, I have honed my skills in front-end technologies like React and Javascript.
+. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile Front-End developer with a passion for creating efficient and user-friendly web applications. With 1.5 years of professional experience, I have worked with a variety of technologies, including React and Javascript.
+My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2021 - Present",
+    role: "Associate Web Developer",
+    company: "INBOUNDSYS WEBGALAXY PRIVATE LIMITED",
+    description: `Led a team in developing and maintaining web applications using HTML ,CSS  and JavaScript.
+    Experienced in development of Web applications using with JavaScript. Excellent Troubleshooting and Problem-solving skills. 
+    Strong Knowledge on ReactJS, HTML5, CSS3, Bootstrap and JavaScript. Certified Hubspot CMS Developer.`,
+    technologies: ["Javascript", "React.js", "HTML", "CSS","Tailwind", "Material-UI","Bootstrap","Hubsspot CMS"], 
+  },
+  {
+    year: "2019 - 2020",
+    role: "Planning and Building",
+    company: "SPML Infra Pvt Ltd",
+    description: `Oversaw subcontract tendering and estimation processes, facilitating the planning and execution of the “Operation and Management of 24x7 WSS to Ballari City Corporation’.Managed reconciliation of all the subcontractor bills, cultivating positive relationships with vendors and clients to ensure timely payments.Administered invoice payments and maintained accurate records in the accounting database.`,
+  }
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing shopping cart",
+    technologies: ["HTML", "CSS"],
+  },
+  {
+    title: "Food Delivery App",
+    image: project2,
+    description:
+      "An app for savoring success: Seamlessly manage orders, deliveries, and customer satisfaction, all in one delicious platform.",
+    technologies: ["HTML", "CSS", "React.js", "Javascript"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "TailwindCss"],
+  } 
+  
+];
+
+export const CONTACT = {
+  address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+12 4555 666 00 ",
+  email: "dhanu241097@gmail.com",
+};
